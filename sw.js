a613ab2176a7e126c5063c39182e023527e9a596
moazzamazmi333-ui <moazzamazmi333@gmail.com>
2026-01-10 02:27:14 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+/* Service Worker for BookGadi */
 self.addEventListener('install', (e) => {
   console.log('Service Worker: Installed');
 });
@@ -5,3 +6,4 @@ self.addEventListener('install', (e) => {
 self.addEventListener('fetch', (e) => {
   // Wix files ko fetch karne ke liye empty handler
 });
+
