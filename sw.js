d55e23f9f5bdcc55a748545f54278eb550a4982d
moazzamazmi333-ui <moazzamazmi333@gmail.com>
2026-01-10 03:07:40 +0530

    Refactor service worker for installation and fetching

--- sw.js
+++ sw.js
@@ -1,9 +1,8 @@
-/* Service Worker for BookGadi */
 self.addEventListener('install', (e) => {
-  console.log('Service Worker: Installed');
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', (e) => {
-  // Wix files ko fetch karne ke liye empty handler
+  // Wix files fetch karne ke liye basic handler
 });
 
