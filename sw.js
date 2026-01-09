ac9fd39c0d508aa0216b2ac6ad960a6bf8d37c69
moazzamazmi333-ui <moazzamazmi333@gmail.com>
2026-01-10 03:42:35 +0530

    Enhance sw.js with comments for clarity
    Added comment to clarify fetch event handling.

--- sw.js
+++ sw.js
@@ -3,5 +3,6 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
+  // Ye code app ko force karega ki wo hamesha internet se naya content (Wix site) uthaye
   event.respondWith(fetch(event.request));
 });
